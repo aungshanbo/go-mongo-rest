cf0fc698a564ffee01b2a2dc1c306ae5137e0f0e
aungshanbo <aungshanbo>
2025-06-04 21:27:26 +0630

    code push

--- main.go
+++ main.go
@@ -15,13 +15,30 @@ func main() {
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
-	http.ListenAndServe("localhost:8080", r)
+	http.ListenAndServe("localhost:9000", r)
+	// if err := http.ListenAndServe(":8080", r); err != nil {
+	// 	log.Fatalf("Server failed to start: %v", err)
+	// }
+
 }
 
 func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27107")
+	s, err := mgo.Dial("mongodb://127.0.0.1:27017")
 	if err != nil {
 		panic(err)
 	}
+
 	return s
+	// info := &mgo.DialInfo{
+	// 	Addrs:    []string{"127.0.0.1:27017"},
+	// 	Username: "users",
+	// 	Password: "users",
+	// 	Database: "mongo-golang",
+	// }
+
+	// session, err := mgo.DialWithInfo(info)
+	// if err != nil {
+	// 	log.Fatalf("MongoDB Connection failed: %v", err)
+	// }
+	// return session
 }
